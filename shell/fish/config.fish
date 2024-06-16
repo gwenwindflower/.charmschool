00ddcd51a42284d1a598ba4c4fb0c49bbb6a766d
gwen windflower <gwenwindflower@gmail.com>
2024-06-16 13:34:45 -0500

    chore(fzf): polish fzf configs

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -55,25 +55,8 @@ source $HOME/.cargo/env.fish
 # Load my fish aliases
 source $HOME/.config/fish/aliases.fish
 
+# Configure tools
+source $HOME/.config/fish/tool_config.fish
+
 # Load private environment variables
 source $HOME/.env.fish
-
-# Set the theme for bat
-set -x -g BAT_THEME Catppuccin-frappe
-
-# Set the theme for fzf
-set -Ux FZF_DEFAULT_OPTS "\
---color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
---color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
---color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284"
-
-# fzf integration config
-fzf_configure_bindings --directory=\cf
-set fzf_directory_opts --bind 'ctrl-o:execute(nvim {} &> /dev/tty),ctrl-n:preview-down,ctrl-p:preview-up'
-set fzf_fd_opts --hidden
-set fzf_preview_file_cmd bat --style=numbers,changes --color always
-set fzf_preview_dir_cmd eza --all --color=always
-set fzf_diff_highlighter delta --paging=never --width=20
-
-# Set the the theme for tools that use LS_COLORS (fd, tree, etc)
-set -Ux LS_COLORS (vivid generate catppuccin-frappe)
