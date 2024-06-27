86e1ebd8b6d83e7fa73be31f14dab24815d404e0
gwen windflower <91998347+gwenwindflower@users.noreply.github.com>
2024-06-26 21:51:40 -0500

    test(fish): add test for more than 1 arg to v func

--- shell/fish/functions/v.fish
+++ shell/fish/functions/v.fish
@@ -1,6 +1,8 @@
 function v -d "If supplied a directory, z to it and open nvim, otherwise just open nvim in the current directory"
     if test (count $argv) -eq 0
         nvim
+    else if test (count $argv) -gt 1
+        echo "Too many arguments. Usage: v [directory]"
     else
         z $argv[1]; and nvim
     end
