db8cd532857ca4c9c146eac82ebcda889caede26
gwen windflower <91998347+gwenwindflower@users.noreply.github.com>
2024-06-26 21:52:03 -0500

    feat(fish): add ripgrep+fzf integrated functions

---
+++ shell/fish/functions/rf.fish
@@ -0,0 +1,11 @@
+function rf -d "Launch fzf as an interface to ripgrep"
+    set RG_PREFIX "rg --column --line-number --no-heading --color=always --smart-case "
+    set INITIAL_QUERY (string join " " $argv)
+    fzf --ansi --disabled --query "$INITIAL_QUERY" \
+        --bind "start:reload:$RG_PREFIX {q}" \
+        --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
+        --delimiter : \
+        --preview 'bat --color=always {1} --highlight-line {2}' \
+        --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
+        --bind 'enter:become(vim {1} +{2})'
+end
---
+++ shell/fish/functions/sr.fish
@@ -0,0 +1,9 @@
+function sr -d "Search for a string in files using ripgrep then filter files with fzf"
+    rg --color=always --line-number --no-heading --smart-case (string join " " $argv) |
+        fzf --ansi \
+            --color "hl:-1:underline,hl+:-1:underline:reverse" \
+            --delimiter : \
+            --preview 'bat --color=always {1} --highlight-line {2}' \
+            --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
+            --bind 'enter:become(vim {1} +{2})'
+end
