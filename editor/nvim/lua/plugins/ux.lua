f617111ddf2a0a4c22e8d9bd13c15f3d6e2d5c5f
gwen windflower <91998347+gwenwindflower@users.noreply.github.com>
2024-06-30 10:07:49 -0500

    feat(nvim): update floating terminal border

--- editor/nvim/lua/plugins/ux.lua
+++ editor/nvim/lua/plugins/ux.lua
@@ -39,7 +39,7 @@ return {
       version = "*",
       opts = {
         float_opts = {
-          border = "curved",
+          border = "double",
           width = 100,
           height = 40,
           winblend = 20,
