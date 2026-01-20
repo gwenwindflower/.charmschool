b5219327d4bf56cf2946b91106a573c4e80fcbc9
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-01-19 22:59:47 -0600

    chore(fish): improve and simplify git plugins and abbrs

--- shell/fish/00_plugin_config.fish
+++ shell/fish/00_plugin_config.fish
@@ -2,8 +2,11 @@
 # Because plugins are sourced alphabetically from conf.d
 # *before* config.fish - any plugin-specific config has to be here
 
-# I prefer to use forgit as a git subcommand
-# and define aliases in gitconfig
+# forgit's shell aliases clash with the extremely comprehensive
+# git fish plugin, which is quite helpful, so instead of these
+# we have git subcommand aliases set in gitconfig like `git fga`, `git fgct`, etc.
+# I then have my own abbreviations for those commands in the `gfg` namespace
+# `gfga`, `gfgct`, etc
 set -gx FORGIT_NO_ALIASES 1
 
 set -gx FORGIT_GLO_FORMAT "%C(green)%h %C(reset)%d %s %C(magenta)%cr%C(reset)"
