70eead352a08903080a6103f0c1c312f80e20cc5
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-02-07 16:03:17 -0600

    chore(fish): improve ai agent config env vars
    Claude Code env var configs can be defined in settings.json, so we're
    able to delete DISABLE_AUTOUPDATER, then add some config flags for
    OpenCode

--- shell/fish/user_conf/03-ai.fish
+++ shell/fish/user_conf/03-ai.fish
@@ -2,4 +2,7 @@
 
 # Claude Code doesn't use this, but it's helpful for me
 set -gx CLAUDE_HOME $HOME/.claude
-set -gx DISABLE_AUTOUPDATER 1
+
+# OpenCode settings
+set -gx OPENCODE_EXPERIMENTAL_LSP_TOOL 1
+set -gx OPENCODE_ENABLE_EXA 1
