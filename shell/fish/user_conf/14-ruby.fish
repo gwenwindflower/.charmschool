1804227acf3797b5991962a7dedf9c561682f042
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-02-10 22:04:11 -0600

    feat(ruby): add rv comprehensive ruby tool
    rv is like uv for ruby. fast and rust-based, with an eye towards
    unifying the entire toolchain. it is still in early development, so we
    need to keep the brew global installs around, but this will fairly
    safely ensure we still install rv and latest v4 via rv to mess with.

--- shell/fish/user_conf/14-ruby.fish
+++ shell/fish/user_conf/14-ruby.fish
@@ -1,1 +1,5 @@
 fish_add_path $HOMEBREW_PREFIX/opt/ruby/bin
+# NOTE: rv is in early development, and still missing a lot of features
+# so we keep Homebrew Ruby cooking above, and setup rv below
+# will shift over to rv as it matures
+$HOMEBREW_PREFIX/bin/rv shell init fish | source
