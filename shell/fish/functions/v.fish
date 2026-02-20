ad41741d8526cbd3733f111d6762f3bce9a228c5
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-02-19 21:32:26 -0600

    chore(fish): update v function to use opo and logirl
    v has been around a long time, this updates its logging to use logirl
    and calls nvim via opo for more env security

--- shell/fish/functions/v.fish
+++ shell/fish/functions/v.fish
@@ -1,18 +1,21 @@
-function v -d "If supplied directory search terms, z to it and open nvim, otherwise just open nvim in the current directory"
+function v -d "Navigate with zoxide to common directories and launch nvim"
     if test (count $argv) -eq 0
-        nvim
+        opo nvim
     end
     if test (count $argv) -eq 1
         if test -f $argv[1]
-            nvim $argv[1]
+            opo nvim $argv[1]
         else
-            z $argv[1]; and nvim
+            # z is instantiated by zoxide's shell integration,
+            # so fish-lsp can't see it
+            # @fish-lsp-disable-next-line
+            z $argv[1]; and opo nvim
         end
     end
     if test (count $argv) -gt 1
-        echo $(set_color -o red)"[ERROR]$(set_color normal)$(set_color red): Too many arguments supplied.$(set_color normal) Please provide only one directory to z into or file name to open directly."
-        echo $(set_color green)"Usage: v [directory or file name]$(set_color normal)"
-        echo "Flexible nvim launcher, uses zoxide to quickly navigate to directories, directly opens files, or launches nvim in current working directory if called without arguments."
+        logirl error "Too many arguments supplied.$(set_color normal) Please provide only one directory to z into or file name to open directly."
+        logirl help_usage "v $(set_color brmagenta)[existing path]$(set_color normal)"
+        logirl "Flexible nvim launcher, uses zoxide to quickly navigate to directories, directly opens files, or launches nvim in current working directory if called without arguments."
         return 1
     end
 end
