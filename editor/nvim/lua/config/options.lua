46bc0fa12d1c05432fa0e277fc40856218ca367e
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-02-25 09:58:40 -0600

    chore(nvim): set spellfile path explicitly
    This supports adding rare words, which have no default fallback and thus
    don't work without a spellfile being specified.

--- editor/nvim/lua/config/options.lua
+++ editor/nvim/lua/config/options.lua
@@ -9,3 +9,7 @@ vim.g.lazyvim_prettier_needs_config = true
 vim.g.lazyvim_picker = "snacks"
 vim.g.lazyvim_python_ruff = "ruff"
 vim.g.snacks_animate = true
+-- Set the spellfile explicitly to the standard path so we can add rare words
+-- zg works a bit more magically, it will create a spellfile here if doesn't exist
+-- spellrare will not, so it fails if this is not specified
+vim.opt.spellfile = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
