c67b8a67807bce9df4ed478746cc629c2cc91b73
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-02-26 11:40:42 -0600

    chore(fish): 🔧 update claude code launch binding to use op

--- shell/fish/functions/claude_launch.fish
+++ shell/fish/functions/claude_launch.fish
@@ -1,5 +1,5 @@
 function claude_launch -d "Launch Claude Code then clean up afterwards, for use with keybindings"
     printf "$(set_color --bold magenta) Launching Claude Code...$(set_color normal)\n"
-    command claude
+    opo claude
     commandline -f repaint
 end
