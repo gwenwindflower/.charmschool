b490fc5450ac380ebe95a9e7551d8831001a6093
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-02-27 10:45:59 -0600

    chore(nvim): 🔧 remove stale and dupe theme settings

--- editor/nvim/lua/plugins/colors.lua
+++ editor/nvim/lua/plugins/colors.lua
@@ -20,30 +20,23 @@ return {
             enabled = true,
             indent_scope_color = "pink",
           },
-          markview = true,
           blink_cmp = {
             enabled = true,
             style = "bordered",
           },
+          auto_integrations = true,
         },
         custom_highlights = function(colors)
           return {
-            -- softens the red hl for selected search match
-            -- CurSearch = { fg = colors.mantle, bg = colors.maroon },
-            -- blink.cmp integration is (i think) still a WIP,
-            -- this is just to temporarily make it consistent with other float windows like Snacks Picker
+            -- TODO: contribute these back to catppuccin/nvim
+            -- these make blink.cmp consistent with the nvchad style of
+            -- float.transparent + float.solid
             Pmenu = { bg = "NONE" },
             BlinkCmpMenuBorder = { link = "FloatBorder" },
-            BlinkCmpDocBorder = { link = "FloatBorder" },
             BlinkCmpScrollBarGutter = { bg = "NONE" },
             BlinkCmpLabel = { fg = colors.subtext0 },
             -- making the underline pop a little more while still looking natural
-            TreesitterContextBottom = { underline = true, sp = colors.surface2, cterm = { underline = true } },
-            -- darken inactive buffer 'tabs' a bit to more easily parse active buffer
-            -- BufferLineBackground = {
-            --   default = true,
-            --   fg = colors.overlay0,
-            -- },
+            TreesitterContextBottom = { underline = true, sp = colors.overlay0, cterm = { underline = true } },
           }
         end,
       })
