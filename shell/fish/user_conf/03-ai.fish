64b6dc8914c526458570d9631fdfee520a76bef6
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-03-02 10:56:28 -0600

    chore(fish): 🧰 add comment on claude env var settings

--- shell/fish/user_conf/03-ai.fish
+++ shell/fish/user_conf/03-ai.fish
@@ -1,6 +1,7 @@
 # AI tools setup
 
 # Claude Code doesn't use this, but it's helpful for me
+# Claude's env var settings are in ~/.claude/settings.json>"env":{}
 set -gx CLAUDE_HOME $HOME/.claude
 
 # OpenCode settings
