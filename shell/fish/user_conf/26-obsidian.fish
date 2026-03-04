4c54b5dc95d6dda567b47072d94d8824f54ac33d
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-03-03 21:58:15 -0600

    feat(fish): ✨ add new obsidian cli to path

---
+++ shell/fish/user_conf/26-obsidian.fish
@@ -0,0 +1,5 @@
+# Obsidian now ships with a CLI that works with a runnin app instance
+# the 'obsidian' bin is at the path below
+fish_add_path /Applications/Obsidian.app/Contents/MacOS
+# Not to be confused with 'obsidian-cli'
+# which operates on static files and doesn't require a running app instance
