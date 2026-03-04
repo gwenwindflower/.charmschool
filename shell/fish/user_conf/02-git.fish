f40d51818f5ce72b709d82b2d142fdfd0c887edb
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-03-03 21:59:16 -0600

    fix(fish): 🐛 fix ordering of forgit setup in git configs

--- shell/fish/user_conf/02-git.fish
+++ shell/fish/user_conf/02-git.fish
@@ -1,14 +1,10 @@
-# Configured in ./git/gitconfig -> ~/.config/git/.gitconfig
+# Configured in git/gitconfig -> ~/.config/git/.gitconfig
 # but some tools look for this env var and git does not it set it by default
 # even when configured in the global git config file
 set -gx GIT_PAGER delta
 
 set -gx DFT_DISPLAY inline
 
-# forgit
-if test -f $HOMEBREW_PREFIX/share/forgit/forgit.plugin.fish
-    source $HOMEBREW_PREFIX/share/forgit/forgit.plugin.fish
-end
 # forgit's shell aliases clash with the extremely comprehensive
 # git fish plugin, which is quite helpful, so instead of these
 # we have git subcommand aliases set in gitconfig like `git fga`, `git fgct`, etc.
@@ -19,4 +15,8 @@ set -gx FORGIT_GLO_FORMAT "%C(green)%h %C(reset)%d %s %C(magenta)%cr%C(reset)"
 # ctrl-d to drop stashes from the forgit stash show fzf viewer
 set -gx FORGIT_STASH_FZF_OPTS '--bind="ctrl-d:reload(git stash drop $(cut -d: -f1 <<<{}) 1>/dev/null && git stash list)"'
 # This allows forgit to be accessed as a git subcommand
+# forgit
+if test -f $HOMEBREW_PREFIX/share/forgit/forgit.plugin.fish
+    source $HOMEBREW_PREFIX/share/forgit/forgit.plugin.fish
+end
 fish_add_path $FORGIT_INSTALL_DIR/bin
