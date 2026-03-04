e12473ffa345d2567f374ca9e567e7a3c5341ad8
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-03-03 22:00:09 -0600

    chore(fish): 🧰 reorganize abbrs

--- shell/fish/user_conf/22-abbrs.fish
+++ shell/fish/user_conf/22-abbrs.fish
@@ -1,21 +1,39 @@
-# shell and editor
+# Abbreviations
+
+# abbrs are the preferred way to handle shortcuts in fish
+# they do everything aliases do with more flexibility and transparency
+# e.g. full command is stored in history, not the abbreviation, which powers more useful completion
+# e.g. expand the abbr then edit one of the flags, instead of making tons of variant aliases
+# use ctrl+space to block expanding the abbr
+# so classic commands I abbr can still be run: `cat>ctrl+space>enter` will run `cat` instead of `bat`
+
+# printing
 abbr --add p echo # p for print
 abbr --add pp bat # pp for pretty print
-abbr --add r rip
+abbr --add cat bat
+# shell
 abbr --add e fresh -r # reload shell
 abbr --add ee fresh -c # clear screen
 abbr --add et fresh -g # navigate to ~ then clear and reload
 abbr --add er fresh # clear and reload
 abbr --add fun functions
-abbr --add top btm # you can use ctrl+space to stop the abbreviation expansion
-abbr --add cat bat # helpful if you really do need the classic tool versus the modern alternative
+# processes
+abbr --add top btm
+abbr --add pps procs
+# dir and file management
 abbr --add cp. "pwd | pbcopy"
 abbr --add mkd mkdir -p
 abbr --add mkt mktemp
+abbr --add r rip
+abbr --add mac macchina
+abbr --add cmx chmod +x # make executable
+abbr --add cmme chmod 700 # give me all permsissions and nobody else any
+# editor
+## opo = run nvim via `op run` with nvim environment file
 abbr --add vi opo nvim
 abbr --add vim opo nvim
 abbr --add nvim opo nvim
-abbr --add zx zex # zoxide to search terms and launch Yazi (or nvim with -e)
+# shell snippets
 ## colorized help via bat
 abbr --add --position anywhere -- --help '--help | bat -plhelp'
 abbr --add --position anywhere -- -h '-h | bat -plhelp'
@@ -23,38 +41,43 @@ abbr --add --position anywhere -- -h '-h | bat -plhelp'
 abbr --add --position anywhere -- qq '>/dev/null'
 ## if you want to discard both stdout and stderr
 abbr --add --position anywhere -- qqq '>/dev/null 2>&1'
-
-# config and dotfiles
+# dotfiles
 abbr --add moi chezmoi
 abbr --add moid chezmoi cd
 abbr --add moie chezmoi edit
 abbr --add moia chezmoi apply
-
-# tooling
+# runners
 abbr --add t task
-abbr --add m mise
-abbr --add mu mise use
-abbr --add mi mise install
-abbr --add mr mise run
-abbr --add mc mise config
-abbr --add mcl mise config list
-abbr --add mcs mise config set
-abbr --add mpth $HOME/.local/share/mise/installs/
-abbr --add mac macchina # stats of my mac? mac -> macchina
-## TUIs
+abbr --add mk make
+# 'lazy' TUIs
 abbr --add lgt lazygit
 abbr --add ldr lazydocker
-abbr --add lsh lazyssh
-abbr --add lku k9s
-## fzf
+# file search/view/explore
 abbr --add f fzf
+## 'fe' function launches yazi
 ## this gives a nice preview for file searches by piping fd into fzf with a preview window
 ## useful if you know you want to search files in a directory
 abbr --add fdf "fd . --color always --hidden --ignore | fzf --preview '_fzf_preview_file {}'"
-## print the default opts in a readable list format
+### print the default opts in a readable list format
 abbr --add fzfopts "echo \$FZF_DEFAULT_OPTS | sed 's/^--//; s/ --/\n/g' | bat"
-
+# listing
+abbr --add l lsd -lAg
+abbr --add ls lsd --classic
+abbr --add ll lsd -l
+abbr --add lg lsd -lg
+abbr --add la lsd -A
+abbr --add lla lsd -lA
+abbr --add lt lsd --tree
+# navigation
+abbr --add s z
+abbr --add ss zex # zoxide > launch yazi/nvim
+abbr --add dots "zex -e $HOME/.charmschool"
+abbr --add conf "zex $XDG_CONFIG_HOME"
+abbr --add proj "zex $PROJECTS"
+abbr --add keeb "zex -e $PROJECTS_UTILS/tinybabykeeb"
 # Homebrew
+## custom function to run commands across multiple fzf-selected formulae/casks
+abbr --add brx brewdo
 ## updates and installs
 abbr --add bri "brew update; brew install"
 abbr --add brr brew uninstall
@@ -62,7 +85,7 @@ abbr --add bru "brew update; brew upgrade; brew cleanup"
 abbr --add brs "brew update; brew search"
 abbr --add brc brew cleanup
 abbr --add brcl brew cleanup
-## list
+## details and dependencies
 abbr --add brl brew list
 abbr --add brl brew list
 abbr --add brlf brew list --installed-on-request
@@ -72,7 +95,6 @@ abbr --add brls brew list
 abbr --add brlsf brew list --installed-on-request
 abbr --add brlsi brew list --installed-on-request
 abbr --add brlsc brew list --cask
-## details and dependencies
 abbr --add br? brew info
 abbr --add brin brew info
 abbr --add brd brew deps
@@ -82,42 +104,35 @@ abbr --add bruise brew uses --installed
 ## background services
 abbr --add brbg brew services
 abbr --add brsrv brew services
-# custom function to run commands across multiple fzf-selected formulae/casks
-abbr --add brx brewdo
-
 # ai
-abbr --add ccf zex $HOME/.claude
-abbr --add cco opo claude
-abbr --add claude opo claude
-abbr --add ccfs "zex $HOME/.charmschool/agents/claude/skills"
-# TODO: set up env files for crush, opencode, copilot, and codex
-abbr --add uwu opo crush
-# abbr --add crush opo crush
 abbr --add opc opo opencode
-# abbr --add opencode opo opencode
-abbr --add cdx opo codex
-# abbr --add codex opo codex
+abbr --add ghc opo copilot
+## claude
+abbr --add cco opo claude
+abbr --add ccf zex $HOME/.claude
+abbr --add ccfs "zex $HOME/.charmschool/agents/claude/skills"
 abbr --add ccu "bunx ccusage@latest"
-
-# writing, notes, and to-dos
+# writing
 ## default Obsidian vault
 abbr --add notes "zex -e '$OBSIDIAN_DEFAULT_VAULT'"
 abbr --add ob obsidian-cli
-abbr --add obsidian obsidian-cli
-
-# mitmproxy
+# security and network
+## 1password
+abbr --add ope "op run --env-file=$OP_ENV_DIR/global.env --no-masking -- " # run with default global env file, without setting env vars for the session, no masking (for interactive use)
+abbr --add opi "op run --no-masking -- "
+abbr --add opr "op run -- "
+abbr --add openv o $OP_ENV_DIR
+## macos keyring
+abbr --add keys security
+## mitmproxy
 abbr -a 'prx?' prxstate
 abbr -a mp mitmproxy
-
-# zoxide quick directory jumping
-## built on top of zex fish function wrapping zoxide -> [nvim | yazi]
-abbr --add s z
-abbr --add dots "zex -e $HOME/.charmschool"
-abbr --add conf "zex $HOME/.config"
-abbr --add dev "zex $HOME/dev"
-abbr --add keeb "zex -e $HOME/dev/04_utils/tinybabykeeb"
-
-# dbt
+# data
+abbr --add dbx databricks
+abbr --add ddb duckdb
+abbr --add pg pgcli
+abbr --add sqli sqlite3
+## dbt
 abbr --add db dbtf build -s
 abbr --add dba dbtf build
 abbr --add dt dbtf test -s
@@ -126,28 +141,17 @@ abbr --add dr dbtf run -s
 abbr --add dra dbtf run
 abbr --add dbtp nvim ~/.dbt/profiles.yml
 abbr --add dbtpp bat ~/.dbt/profiles.yml
-
-# security
-## 1password
-# 'global'
-abbr --add ope "op run --env-file=$OP_ENV_DIR/global.env --no-masking -- " # run with default global env file, without setting env vars for the session, no masking (for interactive use)
-abbr --add opi "op run --no-masking -- "
-abbr --add opr "op run -- "
-abbr --add openv o $OP_ENV_DIR
-abbr --add keys security
-
-# compute and data
-abbr --add dbx databricks
-abbr --add ddb duckdb
-abbr --add pg pgcli
-
-# media and misc
-abbr --add spotify spotify_player
-abbr --add spt spotify_player
-abbr --add ytdl yt-dlp
-abbr --add gdl gallery-dl
-
-# python
+# languages
+## multi-runtime
+abbr --add m mise
+abbr --add mu mise use
+abbr --add mi mise install
+abbr --add mr mise run
+abbr --add mc mise config
+abbr --add mcl mise config list
+abbr --add mcs mise config set
+abbr --add mpth $HOME/.local/share/mise/installs/
+## python
 abbr --add py python
 abbr --add pym python main.py
 abbr --add ip ipython
@@ -170,35 +174,23 @@ abbr --add uvi uv init
 abbr --add uvb uv build
 abbr --add va source .venv/bin/activate.fish
 abbr --add da deactivate
-
-# go
+## go
 abbr --add gor go run main.go
 abbr --add gord go run .
 abbr --add got go test
 abbr --add gotv go test -v
 abbr --add gob go build
-
-# rust
+## rust
 abbr --add ruu rustup up
-
-# lsd
-abbr --add l lsd -lAg
-abbr --add ls lsd --classic
-abbr --add ll lsd -l
-abbr --add lg lsd -lg
-abbr --add la lsd -A
-abbr --add lla lsd -lA
-abbr --add lt lsd --tree
-
-# typescript
-# pnpm
+## typescript
+### pnpm
 abbr --add n pnpm
 abbr --add nx pnpx
 abbr --add ni pnpm i
 abbr --add nu pnpm up
 abbr --add nd pnpm dev
 abbr --add nb pnpm build
-# bun
+### bun
 abbr --add b bun
 abbr --add bi bun i
 abbr --add ba bun add
@@ -206,11 +198,10 @@ abbr --add bu bun upgrade
 abbr --add bs bun start
 abbr --add br bun run
 abbr --add bx bunx
+### deno
 abbr --add bt bun test
-# deno
 abbr --add d deno
 abbr --add dig deno install -grAf --root $DENO_HOME
-
 # git
 abbr --add g git
 ## config
@@ -237,9 +228,13 @@ abbr --add gfgcf git forgit checkout_file
 abbr --add gfgcc git forgit checkout_commit
 abbr --add gfgrc git forgit revert_commit
 abbr --add gfgcl git forgit clean
-
 abbr --add gfgrh git forgit reset_head
 abbr --add gfgss git forgit stash_show
 abbr --add gfgsp git forgit stash_push
 abbr --add gfgcp git forgit cherry_pick
 abbr --add gfgcpb git forgit cherry_pick_from_branch
+# media
+abbr --add spotify spotify_player
+abbr --add spt spotify_player
+abbr --add ytdl yt-dlp
+abbr --add gdl gallery-dl
