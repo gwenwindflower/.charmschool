6c0a147f61a420c1b2c8ccc3ee39074a0ed627e0
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-03-04 10:33:26 -0600

    refactor(fish): ♻️ use kitty ssh kitten in exe func

--- shell/fish/functions/exe.fish
+++ shell/fish/functions/exe.fish
@@ -1,7 +1,15 @@
-function exe -d "SSH into an exe.dev server"
-    if test (count $argv) -ne 1
-        echo "Usage: exe <server>"
+function exe -d "Use Kitty's SSH kitten + agent forwarding to seamlessly connect to an exe.dev VM."
+    argparse h/help -- $argv
+
+    if set -q _flag_help
+        echo "Use Kitty's SSH kitten + agent forwarding to seamlessly connect to an exe.dev VM."
+        logirl help_usage "exe <server-name>"
         return 1
     end
-    ssh -A $argv[1].exe.xyz
+    if test (count $argv) -ne 1
+        echo "Use Kitty's SSH kitten + agent forwarding to seamlessly connect to an exe.dev VM."
+        logirl help_usage "exe <server-name>"
+        return 1
+    end
+    kitten ssh -A $argv[1].exe.xyz
 end
