e37ca09168976b695056b8cb5aa8d379353f59a8
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-03-04 11:33:55 -0600

    chore(fish): 🧰 tidy PATH logic for brew, mise, and obsidian app

--- shell/fish/user_conf/19-mise.fish
+++ shell/fish/user_conf/19-mise.fish
@@ -1,3 +1,12 @@
+# We re-add Homebrew's primary bin dir with 'move' here to bring it to the front
+# of $fish_user_paths just before we add `mise`
+# which needs to come first (and thus be added last)
+# we do this some Homebrew install of tools that get duplicated (primarily by mason.nvim)
+# are first in the path, as they are easier to manage
+# e.g. mason installs 'deno' because it contains its own language server,
+# but Homebrew's deno is easier to manage and upgrade
+# this way we're never wondering why a Homebrew tool is showing an unexpected version
+fish_add_path -g -m $HOMEBREW_PREFIX/bin
 # See notes in shell/fish/user_conf/AGENTS.md about mise auto-activation
 $HOMEBREW_PREFIX/bin/mise activate fish | source
 # Not used by Mise, but useful for quick access to Mise stuff quickly
--- shell/fish/user_conf/26-obsidian.fish
+++ shell/fish/user_conf/26-obsidian.fish
@@ -1,5 +1,10 @@
-# Obsidian now ships with a CLI that works with a runnin app instance
+# Obsidian now ships with a CLI that works with a running app instance
 # the 'obsidian' bin is at the path below
-fish_add_path /Applications/Obsidian.app/Contents/MacOS
-# Not to be confused with 'obsidian-cli'
+# we use the '-a' flag to append it
+# as we don't want it coming before `mise`, our last addition (in 19-mise.fish)
+# it's a very unique and low-priority path (the only bin here is 'obsidian')
+# so it's easiest to just append it rather than break our file patterns
+# by putting it elsewhere
+fish_add_path -a /Applications/Obsidian.app/Contents/MacOS
+# This is not to be confused with 'obsidian-cli', installed by Homebrew
 # which operates on static files and doesn't require a running app instance
