9cd20a070a617e170ed15310861287950f1164c3
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-03-18 00:55:35 -0500

    chore(fish): 🧰 delete claude_launch function

--- shell/fish/functions/claude_launch.fish
+++
@@ -1,5 +0,0 @@
-function claude_launch -d "Launch Claude Code then clean up afterwards, for use with keybindings"
-    printf "$(set_color --bold magenta) Launching Claude Code...$(set_color normal)\n"
-    opo claude
-    commandline -f repaint
-end
