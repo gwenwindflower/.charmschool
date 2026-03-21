12614aabd344fd8844d89bba42084337e7261626
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-03-21 14:36:35 -0500

    chore(fish): remove redundant -g flag from mason path add

--- shell/fish/user_conf/01-editor.fish
+++ shell/fish/user_conf/01-editor.fish
@@ -3,4 +3,4 @@ set -gx NVIM_PLUGIN_DIR $XDG_DATA_HOME/nvim
 set -gx NVIM_MASON_INSTALL $NVIM_PLUGIN_DIR/mason/packages
 # Make mason-installed tools (LSPs, linters, formatters, etc for nvim) available in PATH
 # mason creates a bin dir with symlinks to the actual installed packages in NVIM_MASON_INSTALL
-fish_add_path -g $NVIM_PLUGIN_DIR/mason/bin
+fish_add_path $NVIM_PLUGIN_DIR/mason/bin
