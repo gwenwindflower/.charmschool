76370ead254cd570ead481db8d49f4aa0e089439
winnie <91998347+gwenwindflower@users.noreply.github.com>
2026-03-22 13:30:30 -0500

    refactor(tools)!: source conf files in env vars for fzf + rg

--- shell/fish/user_conf/24-tools.fish
+++ shell/fish/user_conf/24-tools.fish
@@ -1,61 +1,45 @@
-# Set the theme, preview command, and custom bindings for fzf
-# Notes on color themeing:
-# bg/fg is default, bg+/fg+ is selected,
-# hl/hl+ is matched text and matched text on selected line,
-# info is the counter, header is the text *above* the input line,
-# marker is the mark when you multi-select with tab
-set -gx FZF_DEFAULT_OPTS "\
---cycle \
---ansi \
---multi \
---border='rounded' \
---color=border:#737994 \
---scrollbar='█' \
---color=scrollbar:#838ba7 \
---prompt='❯ ' \
---color=prompt:#f4b8e4 \
---pointer='█' \
---color=pointer:#ef9f76 \
---marker=' ' \
---color=marker:#a6d189 \
---color=gutter:#292c3c \
---color=bg:#303446,fg:#b5bfe2 \
---color=bg+:#414559,fg+:#c6d0f5 \
---color=hl+:#ef9f76,hl:#ea999c \
---color=header:#ca9ee6,info:#a6d189,spinner:#a6d189 \
---bind 'ctrl-p:preview-up' \
---bind 'ctrl-n:preview-down' \
---bind='ctrl-d:preview-page-down' \
---bind='ctrl-u:preview-page-up' \
---bind 'alt-p:toggle-preview' \
---bind 'ctrl-y:execute-silent(printf {} | fish_clipboard_copy)+abort' \
---bind 'ctrl-e:become($EDITOR {} &>/dev/tty)' \
-"
-
-# fzf opts for interactive zoxide
+# fzf
+## default opts are parsed from a config file for easier maintenance
+## Notes on color themeing:
+## bg/fg is default, bg+/fg+ is selected,
+## hl/hl+ is matched text and matched text on selected line,
+## info is the counter, header is the text *above* the input line,
+## marker is the mark when you multi-select with tab
+set -gx FZF_DEFAULT_OPTS "$(cat ~/.config/fzf/fzf.conf | tr '\n' ' ')"
+## fzf opts for interactive zoxide
 set -gx _ZO_FZF_OPTS $FZF_DEFAULT_OPTS"\
 --layout=reverse \
 --height=90% \
 --preview-window=wrap\
 "
-
-# fzf.fish plugin config
-## example of how to configure bindings:
-## include hidden files in the fd command that feeds fzf directory search
+## fzf.fish plugin config
+### include hidden files in the fd command that feeds fzf directory search
 set -gx fzf_fd_opts --hidden
-## fzf preview use bat with color and line numbers for file preview
+### fzf preview use bat with color and line numbers for file preview
 set -gx fzf_preview_file_cmd bat --style=numbers,changes --color always
-## fzf preview use lsd tree view with color for directory preview
+### fzf preview use lsd tree view with color for directory preview
 set -gx fzf_preview_dir_cmd lsd --color=always --group-directories-first --tree --depth=2
-## use delta for diff highlighting in fzf.fish git widgets
+### use delta for diff highlighting in fzf.fish git widgets
 set -gx fzf_diff_highlighter delta --paging=never --width=20
-# forgit is better than the fzf.fish git widgets, so i disable those bindings
-## ctrl-y to copy the value of the env var to the clipboard
+### ctrl-y to copy the value of the env var to the clipboard
 set -gx fzf_variables_opts --bind "\
 ctrl-y:execute-silent( \
   echo {} \
   | xargs -I{} sh -c '"'eval printf '%s' \$$0'"' {} \
   | fish_clipboard_copy \
 )+abort"
+### easier editor launch when using primary file search widget
 set -gx fzf_directory_opts --bind 'enter:become($EDITOR {} &>/dev/tty)'
+### forgit is better than the fzf.fish git widgets, so i disable those bindings
+### they can be called manually if needed
+### also assign variable search a more consistent mnemonic bind
 fzf_configure_bindings --variables='ctrl-alt-v' --git_log= --git_status=
+
+# zoxide
+zoxide init fish | source
+
+# ripgrep
+## ripgrep does not automatically look for config, but the upside is
+## we can put it wherever we want
+## it's just command line flags like bat's config
+set -gx RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/ripgrep.conf"
--- shell/fish/user_conf/24-zoxide.fish
+++
@@ -1,2 +0,0 @@
-# zoxide
-zoxide init fish | source
